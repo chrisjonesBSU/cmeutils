10a2cef75675dbf2dd5dca6766c6a2245d02f608
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-05-11 14:25:25 -0600

    add sampling funcs

--- cmeutils/signac_utils.py
+++ cmeutils/signac_utils.py
@@ -10,12 +10,9 @@ def sample_job(
         threshold_neff,
         conservative=True
 ):
-
     log_file = np.genfromtxt(job.fn(filename), names=True)
-    try:
-        data = log_file[varialbe]
-    except ValueError:
-        print(f"{variable} is not a valid header in {filename}.")
+    col_name = _parse_hoomd_log_names(variable, log_files.dtype.names)
+    data = log_file[col_name]
 
     uncorr_sample, uncorr_indices, prod_start, ineff, Neff = equil_sample(
             data=data,
@@ -24,8 +21,34 @@ def sample_job(
             conservative=conservative
     )
 
+    try:
+        job.doc.samples
+    except KeyError:
+        job.doc.samples = {}
 
-def is_equilibrated(
+    varaible_dict = {
+            "start": uncorr_indices.start + prod_start,
+            "stop": uncorr_indices.stop + prod_start,
+            "step": uncorr_indices.step,
+            "N_samples": Neff
+    }
+    job.doc.samples[variable] = variable_dict
+
+
+def get_sample(job, variable):
+    log_file = np.genfromtxt(job.fn(filename), names=True)
+    col_name = _parse_hoomd_log_names(variable, log_files.dtype.names)
+    data = log_file[col_name]
+
+    indices = range(
+            job.doc.samples[variable]["start"],
+            job.doc.samples[variable]["stop"], 
+            job.doc.samples[variable]["step"], 
+    )
+    return data[indices]
+
+
+def check_equilibration(
         job,
         filename,
         variable,
@@ -34,15 +57,28 @@ def is_equilibrated(
         strict
 ):
     log_file = np.genfromtxt(job.fn(filename), names=True)
-    try:
-        data = log_file[varialbe]
-    except ValueError:
-        print(f"{variable} is not a valid header in {filename}.")
+    col_name = _parse_hoomd_log_names(variable, log_files.dtype.names)
+    data = log_file[col_name]
 
     equilibrated, t0, g, Neff = is_equilibrated(
             data=data,
             threshold_fraction=threshold_fraction,
             threshold_neff=threshold_neff,
-            strict=strict
     )
     return equilibrated
+
+
+def _prase_hoomd_log_names(variable, names):
+    name_match = [i for i in names if variable in i]
+    if len(name_match) == 0:
+        raise ValueError(
+               f"{variable} did not return any matches "
+               "in the log file column names." 
+               f"The possible column names are {names}."
+        )
+    if len(name_match) > 1:
+        raise ValueError(
+                f"The variable {variable} returned multiple column headers "
+                f"from {filename}."
+        )
+    return name_match[0]
