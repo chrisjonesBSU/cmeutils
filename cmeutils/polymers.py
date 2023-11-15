5c547cfd75e1ad32af8db3397e7e2d2bd1e5dc46
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-11-15 14:52:00 -0700

    add get_bond_vectors func

--- cmeutils/polymers.py
+++ cmeutils/polymers.py
@@ -1,20 +1,44 @@
-import warnings
-
-import freud
 import gsd
 import gsd.hoomd
 import MDAnalysis as mda
-from MDAnalysis.analysis import polymer
 import numpy as np
-import rowan
-from rowan import vector_vector_rotation
+from MDAnalysis.analysis import polymer
 
 from cmeutils import gsd_utils
 from cmeutils.gsd_utils import get_molecule_cluster
-from cmeutils.geometry import (
-        get_plane_normal, angle_between_vectors, dihedral_angle
-)
-from cmeutils.plotting import get_histogram
+
+
+def get_bond_vectors(snapshot, bond_types):
+    """Get all normalized bond vectors of matching bond types.
+
+    Parameters
+    ---------
+    snapshot : gsd.hoomd.Frame, required
+        Frame of the GSD trajectory file to use
+    bond_types : list-like, required
+        List of bond types to find bond vectors
+        Choose from options found in gsd.hoomd.Frame.bonds.types
+
+    Returns
+    ------
+    vectors : List of arrays (shape=(1,3)
+        List of all normalized bond vectors matching bond_types
+
+    """
+    vectors = []
+    for bond in bond_types:
+        if bond not in snapshot.bonds.types:
+            raise ValueError(
+                f"Bond type {bond} not found in snapshot.bonds.types"
+            )
+        bond_id = snapshot.bonds.types.index(bond)
+        bond_indices = np.where(snapshot.bonds.typeid == bond_id)[0]
+        for i in bond_indices:
+            bond_group = snapshot.bonds.group[i]
+            p1 = snapshot.particles.position[bond_group[0]]
+            p2 = snapshot.particles.position[bond_group[1]]
+            vectors.append((p2 - p1) / np.linalg.norm(p2 - p1))
+    return vectors
 
 
 def radius_of_gyration(gsd_file, start=0, stop=-1):
@@ -42,7 +66,7 @@ def radius_of_gyration(gsd_file, start=0, stop=-1):
     rg_values = []
     rg_means = []
     rg_std = []
-    for snap in trajectory[start: stop]:
+    for snap in trajectory[start:stop]:
         clusters, cl_props = gsd_utils.get_molecule_cluster(snap=snap)
         rg_values.append(cl_props.radii_of_gyration)
         rg_means.append(np.mean(cl_props.radii_of_gyration))
@@ -77,10 +101,10 @@ def end_to_end_distance(gsd_file, head_index, tail_index, start=0, stop=-1):
     vectors : List of arrays
         The Re vector for each chain for every frame
     """
-    re_array = [] # distances (List of arrays)
-    re_means = [] # mean re distances
-    re_stds = [] # std of re distances
-    vectors = [] # end-to-end vectors (List of arrays)
+    re_array = []  # distances (List of arrays)
+    re_means = []  # mean re distances
+    re_stds = []  # std of re distances
+    vectors = []  # end-to-end vectors (List of arrays)
     with gsd.hoomd.open(gsd_file) as traj:
         for snap in traj[start:stop]:
             unwrap_adj = snap.particles.image * snap.configuration.box[:3]
@@ -98,34 +122,15 @@ def end_to_end_distance(gsd_file, head_index, tail_index, start=0, stop=-1):
                 snap_re_distances[idx] = np.linalg.norm(vec)
 
             re_array.append(snap_re_vectors)
-            re_means.append(np.mean(snap_re_distances)) 
+            re_means.append(np.mean(snap_re_distances))
             re_stds.append(np.std(snap_re_distances))
             vectors.append(snap_re_vectors)
     return (np.array(re_means), np.array(re_stds), re_array, vectors)
 
 
-def nematic_order_param(vectors, director):
-    """Finds the nematic (S2) order parameter for a list of vectors
-
-    Parameters
-    ----------
-    vectors : sequence of vectors; required
-        The list of vectors to use in the nematic order parameter calculation
-    director : numpy.ndarray, shape=(1,3)
-        The nematic director of the reference state
-
-    Returns
-    -------
-    freud.order.Nematic
-    """
-    vectors = np.asarray(vectors)
-    orientations = rowan.normalize(np.append(np.zeros((vectors.shape[0], 1)), vectors, axis=1))
-    nematic = freud.order.Nematic(np.asarray(director))
-    nematic.compute(orientations)
-    return nematic
-
-
-def persistence_length(gsd_file, select_atoms_arg, window_size, start=0, stop=1):
+def persistence_length(
+    gsd_file, select_atoms_arg, window_size, start=0, stop=1
+):
     """Performs time-average sampling of persistence length using MDAnalysis
 
     See:
@@ -150,10 +155,12 @@ def persistence_length(gsd_file, select_atoms_arg, window_size, start=0, stop=1)
         try:
             u = mda.Universe(gsd_file)
             chains = u.atoms.fragments
-            backbones = [chain.select_atoms(select_atoms_arg) for chain in chains]
+            backbones = [
+                chain.select_atoms(select_atoms_arg) for chain in chains
+            ]
             sorted_backbones = [polymer.sort_backbone(bb) for bb in backbones]
             _pl = polymer.PersistenceLength(sorted_backbones)
-            pl = _pl.run(start=frame, stop=sampling_windows[idx+1] - 1)
+            pl = _pl.run(start=frame, stop=sampling_windows[idx + 1] - 1)
             lp_results.append(pl.results.lp)
         except IndexError:
             pass
